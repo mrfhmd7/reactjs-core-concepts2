bc46bd3c3a0e0119f8373ad9ab3d0510db5e3900
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-24 02:04:06 +0600

    useState and useEffect function added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className='App'>
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+};
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+  return (
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+};
+
+function User(props) {
+  return (
+    <div style={{border:'3px solid tomato', margin:'10px', backgroundColor:'skyblue', borderRadius:'10px'}}>
+      <h3>Name: {props.name}</h3>
+      <h4>Email: {props.email}</h4>
     </div>
   );
 };
@@ -24,7 +52,7 @@ function Counter() {
     <div >
       <h1>Count: {count}</h1>
       <button onClick={increaseCount}>Increase </button>
-      <br/>
+      <br />
       <button onClick={decreaseCount}>Decrease</button>
     </div>
   );
